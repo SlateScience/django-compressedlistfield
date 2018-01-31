758b2fecb83436f03c9e8a6e5cc0ec42ae034ab4
benny daon <bennydaon@gmail.com>
2018-01-31 16:31:04 +0200

    using corrent initialization in tests

--- compressedlistfield/tests.py
+++ compressedlistfield/tests.py
@@ -15,18 +15,20 @@ class TestCompressedListField(TestCase):
     demo_model = DemoModel
 
     def test_create(self):
-        obj = self.demo_model.objects.create(cl=[1, 2, 3])
+        obj = self.demo_model.objects.create(cl=AutoGrowingList([1, 2, 3]))
         new_obj = self.demo_model.objects.get(id=obj.id)
 
         self.assertEqual(obj.cl, new_obj.cl)
         self.assertEqual(obj.cl, [1, 2, 3])
 
     def test_extend_list(self):
-        obj = self.demo_model.objects.create(cl=[1, 2, 3])
+        obj = self.demo_model.objects.create(cl=AutoGrowingList([1, 2, 3]))
         obj.cl[1000] = 4
         obj.save()
         new_obj = self.demo_model.objects.get(id=obj.id)
-        self.assertEqual(new_obj.cl[1:4], [1, 2, 3])
+        self.assertEqual(new_obj.cl[1], [1])
+        self.assertEqual(new_obj.cl[2], [2])
+        self.assertEqual(new_obj.cl[3], [3])
         for i in new_obj.cl[4:-1]:
             self.assertEqual(i, AutoGrowingList.EMPTY)
 
